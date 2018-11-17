937b14ad0b0b38af2c38e429a1fea86060432e2d
Gonzalo Colmenarejo <colmenarejo.gon@gmail.com>
2018-11-17 12:15:58 +0100

    Correccion push anterior

---
+++ OnlyWayDown/Assets/Project/Scripts/scr_Snake.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_Snake : MonoBehaviour
+{
+    [SerializeField] private float damageDealt = 1f;
+    public float BounceStrength = 1000f;
+    public Vector2 BounceDir;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<scr_CubeControl>().
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<scr_CubeControl>().DamageMe(damageDealt);
+            BounceDir = -collision.contacts[0].normal;
+            collision.gameObject.GetComponent<scr_CubeControl>().GetPushed(BounceDir, BounceStrength);
+        }
+    }
+}
